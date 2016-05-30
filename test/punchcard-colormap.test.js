18b44bcd73fd3ea7de3489b76d6c370f0df21b54
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-05-30 12:51:39 +0200

    restructure

---
+++ test/punchcard-colormap.test.js
@@ -0,0 +1,21 @@
+var test = require('tape').test;
+var PunchcardColorMap = require('./../../build/js/punchcard.js');
+
+console.log(PunchcardColorMap);
+
+
+test('The add method...', function(t) {
+
+    // var actual,
+    //     expected;
+    //
+    // actual = add(1, 2);
+    // expected = 4;
+
+    t.pass('dummy test');
+
+    // notify tape that there are no more tests
+    t.end();
+
+})
+
