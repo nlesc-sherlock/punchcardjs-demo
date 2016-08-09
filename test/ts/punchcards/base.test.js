8e7312ba699a5d63e567ab1f0050c0b106ffc9ba
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-08-09 16:19:42 +0200

    added placeholder test file

---
+++ test/ts/punchcards/base.test.js
@@ -0,0 +1,24 @@
+var test = require('tape').test;
+var punchcards = require('./../../../dist/punchcards.js');
+
+
+
+test('punchcards.Base constructor', function(t) {
+
+    var actual,
+        expected;
+
+
+    actual = 1;
+    expected = 1;
+
+
+    // dummy test
+    t.equal(actual, expected, '...should do stuff.')
+
+
+    // notify tape that there are no more tests
+    t.end();
+
+})
+
