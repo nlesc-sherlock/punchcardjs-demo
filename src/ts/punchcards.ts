8ecfb9cf29ce345bccbec0ef34d010c91fb578f8
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-08-10 18:16:30 +0200

    now including typescript test in library itself (all other approaches failed...)

--- src/ts/punchcards.ts
+++ src/ts/punchcards.ts
@@ -10,3 +10,5 @@ export {Legend} from './punchcards/legend';
 export {WeekdayCircle} from './punchcards/weekday-circle';
 export {WeekdayRect} from './punchcards/weekday-rect';
 
+// the tests
+export {BaseTest} from './punchcards/base.test';
