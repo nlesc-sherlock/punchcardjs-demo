e17bf1f7dad64bd3ccaf7c5753a410e57e5fb31f
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-08-18 15:52:58 +0200

    corrected error with importing just one record instead of 5000; added check on the number of records that is loaded; added dom tests for DateRect class; removed any newlines in test descriptors (no longer needed with karma-spec-reporter package)

--- test/ts/punchcards/date-rect.dom.js
+++ test/ts/punchcards/date-rect.dom.js
@@ -17,7 +17,7 @@ describe('punchcards DateRect class...', function() {
         fixtures = fixture.load('date-rect.fixture.html',
             'cityofchicago-police-data.fixture.json');
 
-        data = fixtures[1][0];
+        data = fixtures[1];
         cf = crossfilter(data);
 
     });
@@ -32,7 +32,7 @@ describe('punchcards DateRect class...', function() {
 
 
 
-    it('\n...html fixture should have loaded correctly', function() {
+    it('...html fixture should have loaded correctly', function() {
         var actual, expected;
         actual = fixtures[0][0].outerHTML;
         expected = '<div id="punchcard-date-rect"></div>';
@@ -41,7 +41,7 @@ describe('punchcards DateRect class...', function() {
 
 
 
-    it('\n...json fixture should have loaded correctly', function() {
+    it('...json fixture should have loaded correctly', function() {
         var actual, expected;
         actual = Object.keys(fixtures[1][0]).sort();
         expected = ["casenumber", "datestr", "description", "latitude", "longitude", "primary"];
@@ -50,19 +50,77 @@ describe('punchcards DateRect class...', function() {
 
 
 
-    it('\n...constructor should return an instance of punchcards.DateRect when called with valid arguments', function() {
+    it('...json fixture should have 5000 records', function() {
+        var actual, expected;
+        actual = cf.size();
+        expected = 5000;
+        expect(actual).toEqual(expected);
+    });
 
+
+
+    it('...constructor should return an instance of punchcards.DateRect when called with valid arguments', function() {
         var actual, daterect;
         daterect = new punchcards.DateRect(cf, 'punchcard-date-rect');
         actual = daterect instanceof punchcards.DateRect;
         expect(actual).toBe(true);
+    });
 
+
+
+    it('...after calling .drawHorizontalAxis(), the chart should have an ' +
+        'SVG g element of class "horizontal-axis" attached to it', function() {
+        var actual, daterect;
+        daterect = new punchcards.DateRect(cf, 'punchcard-date-rect');
+        daterect.defineDimensions();
+        daterect.drawSvg();
+        daterect.drawHorizontalAxis();
+        actual = daterect.svg.select('g.horizontal-axis')[0][0];
+        expect(actual).not.toBe(null);
+    });
+
+
+
+    it('...after calling .drawSymbols(), the chart should have an ' +
+        'SVG g element of class "symbol" attached to it', function() {
+        var actual, daterect;
+        daterect = new punchcards.DateRect(cf, 'punchcard-date-rect');
+        daterect.defineDimensions();
+        daterect.drawSvg();
+        daterect.drawHorizontalAxis();
+        daterect.drawVerticalAxis();
+        daterect.drawSymbols();
+        actual = daterect.svg.select('g.symbol')[0][0];
+        expect(actual).not.toBe(null);
+    });
+
+
+
+    it('...after calling .drawSymbols(), the chart should have an ' +
+        'SVG g element of class "symbol" with 139 SVG rects of class ' +
+        '"symbol" attached to it', function() {
+        var actual, daterect, symbols;
+        daterect = new punchcards.DateRect(cf, 'punchcard-date-rect');
+        daterect.defineDimensions();
+        daterect.drawSvg();
+        daterect.drawHorizontalAxis();
+        daterect.drawVerticalAxis();
+        daterect.drawSymbols();
+        symbols = daterect.svg.select('g.symbol').selectAll('rect.symbol')[0];
+        actual = symbols.length;;
+        expect(actual).toEqual(139);
     });
 
 
 
 
-    // it('\n...after calling .drawBox(), the chart should have an ' +
+    // this.drawHorizontalAxis();
+    // this.drawSymbols();
+
+
+
+
+    // it('...after calling .drawBox(), the chart should have an ' +
     //     'SVG g element of class "chartbody-box" attached to it.', function() {
     //
     //     var actual, base;
@@ -79,7 +137,7 @@ describe('punchcards DateRect class...', function() {
     //
     //
     //
-    // it('\n...after calling .drawTitle(), the chart should have an ' +
+    // it('...after calling .drawTitle(), the chart should have an ' +
     //     'SVG g element of class "chartbody-box" with an SVG rect ' +
     //     'element of class "chartbody-box" attached to it.', function() {
     //
