006b477b60448fcb3bdda411d4cc3d30847f3731
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-08-24 11:10:15 +0200

    fixed all jslint and jshint errors

--- test/weekday-rect.dom.js
+++ test/weekday-rect.dom.js
@@ -2,11 +2,15 @@
  * tests requiring DOM manipulation for punchcards.WeekdayRect from dist/punchcards.js
  */
 
-describe('punchcards WeekdayRect class...', function() {
+/*global describe, beforeEach, fixture, crossfilter, afterEach, it, expect, punchcards*/
+
+describe('punchcards WeekdayRect class...', function () {
+    
+    'use strict';
 
     var fixtures, cf;
 
-    beforeEach(function() {
+    beforeEach(function () {
 
         var data;
 
@@ -24,7 +28,7 @@ describe('punchcards WeekdayRect class...', function() {
 
 
 
-    afterEach(function(){
+    afterEach(function () {
         // clean up the DOM
         fixture.cleanup();
         cf = undefined;
@@ -32,7 +36,7 @@ describe('punchcards WeekdayRect class...', function() {
 
 
 
-    it('...html fixture should have loaded correctly', function() {
+    it('...html fixture should have loaded correctly', function () {
         var actual, expected;
         actual = fixtures[0][0].outerHTML;
         expected = '<div id="punchcard-weekday-rect"></div>';
@@ -41,7 +45,7 @@ describe('punchcards WeekdayRect class...', function() {
 
 
 
-    it('...json fixture should have loaded correctly', function() {
+    it('...json fixture should have loaded correctly', function () {
         var actual, expected;
         actual = Object.keys(fixtures[1][0]).sort();
         expected = ["casenumber", "datestr", "description", "latitude", "longitude", "primary"];
@@ -50,7 +54,7 @@ describe('punchcards WeekdayRect class...', function() {
 
 
 
-    it('...json fixture should have 5000 records', function() {
+    it('...json fixture should have 5000 records', function () {
         var actual, expected;
         actual = cf.size();
         expected = 5000;
@@ -59,7 +63,7 @@ describe('punchcards WeekdayRect class...', function() {
 
 
 
-    it('...constructor should return an instance of punchcards.WeekdayRect when called with valid arguments', function() {
+    it('...constructor should return an instance of punchcards.WeekdayRect when called with valid arguments', function () {
         var actual, weekdayrect;
         weekdayrect = new punchcards.WeekdayRect(cf, 'punchcard-weekday-rect');
         actual = weekdayrect instanceof punchcards.WeekdayRect;
@@ -69,50 +73,49 @@ describe('punchcards WeekdayRect class...', function() {
 
 
     it('...after calling .drawHorizontalAxis(), the chart should have an ' +
-        'SVG g element of class "horizontal-axis" attached to it', function() {
-        var actual, weekdayrect;
-        weekdayrect = new punchcards.WeekdayRect(cf, 'punchcard-weekday-rect');
-        weekdayrect.defineDimensions();
-        weekdayrect.drawSvg();
-        weekdayrect.drawHorizontalAxis();
-        actual = weekdayrect.svg.select('g.horizontal-axis')[0][0];
-        expect(actual).not.toBe(null);
-    });
+        'SVG g element of class "horizontal-axis" attached to it', function () {
+            var actual, weekdayrect;
+            weekdayrect = new punchcards.WeekdayRect(cf, 'punchcard-weekday-rect');
+            weekdayrect.defineDimensions();
+            weekdayrect.drawSvg();
+            weekdayrect.drawHorizontalAxis();
+            actual = weekdayrect.svg.select('g.horizontal-axis')[0][0];
+            expect(actual).not.toBe(null);
+        });
 
 
 
     it('...after calling .drawSymbols(), the chart should have an ' +
-        'SVG g element of class "symbol" attached to it', function() {
-        var actual, weekdayrect;
-        weekdayrect = new punchcards.WeekdayRect(cf, 'punchcard-weekday-rect');
-        weekdayrect.defineDimensions();
-        weekdayrect.drawSvg();
-        weekdayrect.drawHorizontalAxis();
-        weekdayrect.drawVerticalAxis();
-        weekdayrect.drawSymbols();
-        actual = weekdayrect.svg.select('g.symbol')[0][0];
-        expect(actual).not.toBe(null);
-    });
+        'SVG g element of class "symbol" attached to it', function () {
+            var actual, weekdayrect;
+            weekdayrect = new punchcards.WeekdayRect(cf, 'punchcard-weekday-rect');
+            weekdayrect.defineDimensions();
+            weekdayrect.drawSvg();
+            weekdayrect.drawHorizontalAxis();
+            weekdayrect.drawVerticalAxis();
+            weekdayrect.drawSymbols();
+            actual = weekdayrect.svg.select('g.symbol')[0][0];
+            expect(actual).not.toBe(null);
+        });
 
 
 
     it('...after calling .drawSymbols(), the chart should have an ' +
         'SVG g element of class "symbol" with 139 SVG rects of class ' +
-        '"symbol" attached to it', function() {
-        var actual, weekdayrect, symbols;
-        weekdayrect = new punchcards.WeekdayRect(cf, 'punchcard-weekday-rect');
-        weekdayrect.defineDimensions();
-        weekdayrect.drawSvg();
-        weekdayrect.drawHorizontalAxis();
-        weekdayrect.drawVerticalAxis();
-        weekdayrect.drawSymbols();
-        symbols = weekdayrect.svg.select('g.symbol').selectAll('rect.symbol')[0];
-        actual = symbols.length;;
-        expect(actual).toEqual(139);
-    });
+        '"symbol" attached to it', function () {
+            var actual, weekdayrect, symbols;
+            weekdayrect = new punchcards.WeekdayRect(cf, 'punchcard-weekday-rect');
+            weekdayrect.defineDimensions();
+            weekdayrect.drawSvg();
+            weekdayrect.drawHorizontalAxis();
+            weekdayrect.drawVerticalAxis();
+            weekdayrect.drawSymbols();
+            symbols = weekdayrect.svg.select('g.symbol').selectAll('rect.symbol')[0];
+            actual = symbols.length;
+            expect(actual).toEqual(139);
+        });
 
-
-
-})
+    
+});
 
 
