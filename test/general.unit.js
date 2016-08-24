6070448b3fcf1e9f25c59f80c1af4d17be62e0a9
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-08-24 11:08:04 +0200

    fixed all jslint and jshint errors

--- test/general.unit.js
+++ test/general.unit.js
@@ -2,48 +2,55 @@
  * Tests whether the general testing setup works
  */
 
-describe('punchcards general testing functionality', function() {
+/*global describe, it, expect, punchcards, crossfilter, d3, moment, fixture*/
 
-    it('...should know about Punchcards', function() {
+describe('punchcards general testing functionality', function () {
+    
+    'use strict';
+
+    it('...should know about Punchcards', function () {
         expect(punchcards).not.toBe(null);
     });
 
 
-    it('...should have loaded the crossfilter library', function() {
+    it('...should have loaded the crossfilter library', function () {
         expect(crossfilter).not.toBe(null);
     });
 
 
-    it('...should have loaded the d3 library', function() {
+    it('...should have loaded the d3 library', function () {
         expect(d3).not.toBe(null);
     });
 
 
-    it('...should have loaded the moment library', function() {
+    it('...should have loaded the moment library', function () {
         expect(moment).not.toBe(null);
     });
 
 
-    it('...should have loaded the test data', function() {
+    it('...should have loaded the test data', function () {
+        var data;
         fixture.base = 'test';
-        var data = fixture.load('cityofchicago-police-data.fixture.json');
+        data = fixture.load('cityofchicago-police-data.fixture.json');
         expect(data).not.toBe(null);
     });
 
 
-    it('...should have created a crossfilter object out of the test data', function() {
+    it('...should have created a crossfilter object out of the test data', function () {
+        var data, cf;
         fixture.base = 'test';
-        var data = fixture.load('cityofchicago-police-data.fixture.json');
-        var cf = crossfilter(data);
+        data = fixture.load('cityofchicago-police-data.fixture.json');
+        cf = crossfilter(data);
         expect(cf).not.toBe(null);
     });
 
 
-    it('...should be able to find the html fixtures', function() {
+    it('...should be able to find the html fixtures', function () {
+        var html;
         fixture.base = 'test';
-        var html = fixture.load('base.fixture.html');
+        html = fixture.load('base.fixture.html');
         expect(html).not.toBe(null);
     });
 
 
-})
+});
