6aeb468af150acbdafa1e9cb8715bb4cd660a5fe
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-08-24 10:53:20 +0200

    fixed all jslint errors except about RegExp

--- test/base.dom.js
+++ test/base.dom.js
@@ -2,11 +2,15 @@
  * tests requiring DOM manipulation for punchcards.Base from dist/punchcards.js
  */
 
-describe('punchcards Base class...', function() {
+/*global describe,beforeEach,it,afterEach,crossfilter,fixture,expect,punchcards*/
+
+describe('punchcards Base class...', function () {
+    
+    'use strict';
 
     var fixtures, cf;
 
-    beforeEach(function() {
+    beforeEach(function () {
 
         var data;
 
@@ -24,7 +28,7 @@ describe('punchcards Base class...', function() {
 
 
 
-    afterEach(function(){
+    afterEach(function () {
         // clean up the DOM
         fixture.cleanup();
         cf = undefined;
@@ -32,7 +36,7 @@ describe('punchcards Base class...', function() {
 
 
 
-    it('...html fixture should have loaded correctly', function() {
+    it('...html fixture should have loaded correctly', function () {
         var actual, expected;
         actual = fixtures[0][0].outerHTML;
         expected = '<div id="punchcard-base"></div>';
@@ -41,7 +45,7 @@ describe('punchcards Base class...', function() {
 
 
 
-    it('...json fixture should have loaded correctly', function() {
+    it('...json fixture should have loaded correctly', function () {
         var actual, expected;
         actual = Object.keys(fixtures[1][0]).sort();
         expected = ["casenumber", "datestr", "description", "latitude", "longitude", "primary"];
@@ -50,7 +54,7 @@ describe('punchcards Base class...', function() {
 
 
 
-    it('...json fixture should have 5000 records', function() {
+    it('...json fixture should have 5000 records', function () {
         var actual, expected;
         actual = cf.size();
         expected = 5000;
@@ -59,7 +63,7 @@ describe('punchcards Base class...', function() {
 
 
 
-    it('...constructor should return an instance of punchcards.Base when called with valid arguments', function() {
+    it('...constructor should return an instance of punchcards.Base when called with valid arguments', function () {
         var actual, base;
         base = new punchcards.Base(cf, 'punchcard-base');
         actual = base instanceof punchcards.Base;
@@ -68,7 +72,7 @@ describe('punchcards Base class...', function() {
 
 
 
-    it('...calling .drawSvg() method should add an SVG svg to the DOM.', function() {
+    it('...calling .drawSvg() method should add an SVG svg to the DOM.', function () {
         // should match the string <svg></svg>
         var actual, expected, base, div, re;
         base = new punchcards.Base(cf, 'punchcard-base');
@@ -76,7 +80,7 @@ describe('punchcards Base class...', function() {
         div = fixtures[0][0];
         // capture the svg opening and closing tags using a regular expression
         // see https://regex101.com/ for testing/debugging regular expressions
-        re = new RegExp(/^<svg.*>.*<\/svg>$/)
+        re = new RegExp(/^<svg.*>.*<\/svg>$/);
         actual = re.test(div.innerHTML);
         expected = true;
         expect(actual).toEqual(expected);
@@ -85,75 +89,75 @@ describe('punchcards Base class...', function() {
 
 
     it('...after calling .drawSvg(), the SVG svg element should have a ' +
-        '"width" property with a numerical value.', function() {
-        // use a regular expression to verify that there is a 'width' property
-        // attached to the svg opening tag with a numerical value.
-        // see https://regex101.com/ for testing/debugging regular expressions
-        //
-        // should match the strings
-        //
-        // <svg width='56'></svg>
-        // <svg width="83637"></svg>
-        // <svg width = "83637"></svg>
-        // <svg width="112" height="34566"></svg>
-        // <svg width='112' height='34566'></svg>
-        // <svg height='34566' width='112'></svg>
-        // <svg width='200%' height='100%'></svg>
-        //
-        // but not
-        //
-        // <svg></svg>
-        // <svg width=""></svg>
-        // <svg width="abc"></svg>
-        var actual, expected, base, div, re;
-        base = new punchcards.Base(cf, 'punchcard-base');
-        base.drawSvg();
-        div = fixtures[0][0];
-        // use a regular expression to verify that there is a 'width' property
-        // attached to the svg opening tag with a numerical value.
-        // see https://regex101.com/ for testing/debugging regular expressions
-        re = new RegExp(/^<svg.*width\s*=\s*["']\d{1,}\%?["'].*>.*<\/svg>$/)
-        actual = re.test(div.innerHTML);
-        expected = true;
-        expect(actual).toEqual(expected);
-    });
+        '"width" property with a numerical value.', function () {
+            // use a regular expression to verify that there is a 'width' property
+            // attached to the svg opening tag with a numerical value.
+            // see https://regex101.com/ for testing/debugging regular expressions
+            //
+            // should match the strings
+            //
+            // <svg width='56'></svg>
+            // <svg width="83637"></svg>
+            // <svg width = "83637"></svg>
+            // <svg width="112" height="34566"></svg>
+            // <svg width='112' height='34566'></svg>
+            // <svg height='34566' width='112'></svg>
+            // <svg width='200%' height='100%'></svg>
+            //
+            // but not
+            //
+            // <svg></svg>
+            // <svg width=""></svg>
+            // <svg width="abc"></svg>
+            var actual, expected, base, div, re;
+            base = new punchcards.Base(cf, 'punchcard-base');
+            base.drawSvg();
+            div = fixtures[0][0];
+            // use a regular expression to verify that there is a 'width' property
+            // attached to the svg opening tag with a numerical value.
+            // see https://regex101.com/ for testing/debugging regular expressions
+            re = new RegExp(/^<svg.*width\s*=\s*["']\d{1,}\%?["'].*>.*<\/svg>$/);
+            actual = re.test(div.innerHTML);
+            expected = true;
+            expect(actual).toEqual(expected);
+        });
 
 
 
     it('...after calling .drawSvg(), the SVG svg element should have a ' +
-        '"height" property with a numerical value.', function() {
-        // use a regular expression to verify that there is a 'height' property
-        // attached to the svg opening tag with a numerical value.
-        // see https://regex101.com/ for testing/debugging regular expressions
-        //
-        // should match the strings
-        //
-        // <svg height='56'></svg>
-        // <svg height="83637"></svg>
-        // <svg height = "83637"></svg>
-        // <svg height="112" width="34566"></svg>
-        // <svg height='112' width='34566'></svg>
-        // <svg width='34566' height='112'></svg>
-        // <svg height='200%' width='100%'></svg>
-        //
-        // but not
-        //
-        // <svg></svg>
-        // <svg height=""></svg>
-        // <svg height="abc"></svg>
-        var actual, expected, base, div, re;
-        base = new punchcards.Base(cf, 'punchcard-base');
-        base.drawSvg();
-        div = fixtures[0][0];
-        re = new RegExp(/^<svg.*height\s*=\s*["']\d{1,}\%?["'].*>.*<\/svg>$/)
-        actual = re.test(div.innerHTML);
-        expected = true;
-        expect(actual).toEqual(expected);
-    });
+        '"height" property with a numerical value.', function () {
+            // use a regular expression to verify that there is a 'height' property
+            // attached to the svg opening tag with a numerical value.
+            // see https://regex101.com/ for testing/debugging regular expressions
+            //
+            // should match the strings
+            //
+            // <svg height='56'></svg>
+            // <svg height="83637"></svg>
+            // <svg height = "83637"></svg>
+            // <svg height="112" width="34566"></svg>
+            // <svg height='112' width='34566'></svg>
+            // <svg width='34566' height='112'></svg>
+            // <svg height='200%' width='100%'></svg>
+            //
+            // but not
+            //
+            // <svg></svg>
+            // <svg height=""></svg>
+            // <svg height="abc"></svg>
+            var actual, expected, base, div, re;
+            base = new punchcards.Base(cf, 'punchcard-base');
+            base.drawSvg();
+            div = fixtures[0][0];
+            re = new RegExp(/^<svg.*height\s*=\s*["']\d{1,}\%?["'].*>.*<\/svg>$/);
+            actual = re.test(div.innerHTML);
+            expected = true;
+            expect(actual).toEqual(expected);
+        });
 
 
 
-    it('...after calling .drawSvg(), the instance should have a .svg member.', function() {
+    it('...after calling .drawSvg(), the instance should have a .svg member.', function () {
         var actual, base;
         base = new punchcards.Base(cf, 'punchcard-base');
         base.drawSvg();
@@ -164,141 +168,141 @@ describe('punchcards Base class...', function() {
 
 
     it('...after calling .drawChartBody(), the chart should have ' +
-       'an SVG g element of class "chartbody".', function() {
-        var actual, base;
-        base = new punchcards.Base(cf, 'punchcard-base');
-        base.drawSvg();
-        base.drawChartBody();
-        actual = base.svg.select('g.chartbody');
-        expect(actual).not.toBe(null);
-    });
+         'an SVG g element of class "chartbody".', function () {
+            var actual, base;
+            base = new punchcards.Base(cf, 'punchcard-base');
+            base.drawSvg();
+            base.drawChartBody();
+            actual = base.svg.select('g.chartbody');
+            expect(actual).not.toBe(null);
+        });
 
 
 
     it('...after calling .drawChartBody(), the chart should have an ' +
         'SVG g element of class "chartbody" with an SVG rect element attached ' +
-        'to it.', function() {
-        var actual, base;
-        base = new punchcards.Base(cf, 'punchcard-base');
-        base.drawSvg();
-        base.drawChartBody();
-        actual = base.svg.select('g.chartbody').select('rect.chartbody');
-        expect(actual).not.toBe(null);
-    });
+        'to it.', function () {
+            var actual, base;
+            base = new punchcards.Base(cf, 'punchcard-base');
+            base.drawSvg();
+            base.drawChartBody();
+            actual = base.svg.select('g.chartbody').select('rect.chartbody');
+            expect(actual).not.toBe(null);
+        });
 
 
 
     it('...after calling .drawHorizontalAxisLabel(), the chart should have an ' +
-        'SVG g element of class "horizontal-axis-label" attached to it.', function() {
-        var actual, base;
-        base = new punchcards.Base(cf, 'punchcard-base');
-        base.drawSvg();
-        base.drawHorizontalAxisLabel();
-        actual = base.svg.select('g.horizontal-axis-label')[0][0];
-        expect(actual).not.toBe(null);
-    });
+        'SVG g element of class "horizontal-axis-label" attached to it.', function () {
+            var actual, base;
+            base = new punchcards.Base(cf, 'punchcard-base');
+            base.drawSvg();
+            base.drawHorizontalAxisLabel();
+            actual = base.svg.select('g.horizontal-axis-label')[0][0];
+            expect(actual).not.toBe(null);
+        });
 
 
 
     it('...after calling .drawHorizontalAxisLabel(), the chart should have an ' +
         'SVG g element of class "horizontal-axis-label" with an SVG text ' +
-        'element of class "horizontal-axis-label" attached to it.', function() {
-        var actual, base;
-        base = new punchcards.Base(cf, 'punchcard-base');
-        base.drawSvg();
-        base.drawHorizontalAxisLabel();
-        actual = base.svg.select('g.horizontal-axis-label').select('text.horizontal-axis-label')[0][0];
-        expect(actual).not.toBe(null);
-    });
+        'element of class "horizontal-axis-label" attached to it.', function () {
+            var actual, base;
+            base = new punchcards.Base(cf, 'punchcard-base');
+            base.drawSvg();
+            base.drawHorizontalAxisLabel();
+            actual = base.svg.select('g.horizontal-axis-label').select('text.horizontal-axis-label')[0][0];
+            expect(actual).not.toBe(null);
+        });
 
 
 
     it('...after calling .drawVerticalAxis(), the chart should have an ' +
-        'SVG g element of class "vertical-axis" attached to it.', function() {
-        var actual, base;
-        base = new punchcards.Base(cf, 'punchcard-base');
-        base.drawSvg();
-        base.drawVerticalAxis();
-        actual = base.svg.select('g.vertical-axis')[0][0];
-        expect(actual).not.toBe(null);
-    });
+        'SVG g element of class "vertical-axis" attached to it.', function () {
+            var actual, base;
+            base = new punchcards.Base(cf, 'punchcard-base');
+            base.drawSvg();
+            base.drawVerticalAxis();
+            actual = base.svg.select('g.vertical-axis')[0][0];
+            expect(actual).not.toBe(null);
+        });
 
 
 
     it('...after calling .drawVerticalAxisLabel(), the chart should have an ' +
-        'SVG g element of class "vertical-axis-label" attached to it.', function() {
-        var actual, base;
-        base = new punchcards.Base(cf, 'punchcard-base');
-        base.drawSvg();
-        base.drawVerticalAxisLabel();
-        actual = base.svg.select('g.vertical-axis-label')[0][0];
-        expect(actual).not.toBe(null);
-    });
+        'SVG g element of class "vertical-axis-label" attached to it.', function () {
+            var actual, base;
+            base = new punchcards.Base(cf, 'punchcard-base');
+            base.drawSvg();
+            base.drawVerticalAxisLabel();
+            actual = base.svg.select('g.vertical-axis-label')[0][0];
+            expect(actual).not.toBe(null);
+        });
 
 
 
     it('...after calling .drawVerticalAxisLabel(), the chart should have an ' +
         'SVG g element of class "vertical-axis-label" with an SVG text ' +
-        'element of class "vertical-axis-label" attached to it.', function() {
-        var actual, base;
-        base = new punchcards.Base(cf, 'punchcard-base');
-        base.drawSvg();
-        base.drawVerticalAxisLabel();
-        actual = base.svg.select('g.vertical-axis-label').select('text.vertical-axis-label')[0][0];
-        expect(actual).not.toBe(null);
-    });
+        'element of class "vertical-axis-label" attached to it.', function () {
+            var actual, base;
+            base = new punchcards.Base(cf, 'punchcard-base');
+            base.drawSvg();
+            base.drawVerticalAxisLabel();
+            actual = base.svg.select('g.vertical-axis-label').select('text.vertical-axis-label')[0][0];
+            expect(actual).not.toBe(null);
+        });
 
 
 
     it('...after calling .drawTitle(), the chart should have an ' +
-        'SVG g element of class "title" attached to it.', function() {
-        var actual, base;
-        base = new punchcards.Base(cf, 'punchcard-base');
-        base.drawSvg();
-        base.drawTitle();
-        actual = base.svg.select('g.title')[0][0];
-        expect(actual).not.toBe(null);
-    });
+        'SVG g element of class "title" attached to it.', function () {
+            var actual, base;
+            base = new punchcards.Base(cf, 'punchcard-base');
+            base.drawSvg();
+            base.drawTitle();
+            actual = base.svg.select('g.title')[0][0];
+            expect(actual).not.toBe(null);
+        });
 
 
 
     it('...after calling .drawTitle(), the chart should have an ' +
         'SVG g element of class "title" with an SVG text ' +
-        'element of class "title" attached to it.', function() {
-        var actual, base;
-        base = new punchcards.Base(cf, 'punchcard-base');
-        base.drawSvg();
-        base.drawTitle();
-        actual = base.svg.select('g.title').select('text.title')[0][0];
-        expect(actual).not.toBe(null);
-    });
+        'element of class "title" attached to it.', function () {
+            var actual, base;
+            base = new punchcards.Base(cf, 'punchcard-base');
+            base.drawSvg();
+            base.drawTitle();
+            actual = base.svg.select('g.title').select('text.title')[0][0];
+            expect(actual).not.toBe(null);
+        });
 
 
 
     it('...after calling .drawBox(), the chart should have an ' +
-        'SVG g element of class "chartbody-box" attached to it.', function() {
-        var actual, base;
-        base = new punchcards.Base(cf, 'punchcard-base');
-        base.drawSvg();
-        base.drawBox();
-        actual = base.svg.select('g.chartbody-box')[0][0];
-        expect(actual).not.toBe(null);
-    });
+        'SVG g element of class "chartbody-box" attached to it.', function () {
+            var actual, base;
+            base = new punchcards.Base(cf, 'punchcard-base');
+            base.drawSvg();
+            base.drawBox();
+            actual = base.svg.select('g.chartbody-box')[0][0];
+            expect(actual).not.toBe(null);
+        });
 
 
 
     it('...after calling .drawTitle(), the chart should have an ' +
         'SVG g element of class "chartbody-box" with an SVG rect ' +
-        'element of class "chartbody-box" attached to it.', function() {
-        var actual, base;
-        base = new punchcards.Base(cf, 'punchcard-base');
-        base.drawSvg();
-        base.drawBox();
-        actual = base.svg.select('g.chartbody-box').select('rect.chartbody-box')[0][0];
-        expect(actual).not.toBe(null);
-    });
+        'element of class "chartbody-box" attached to it.', function () {
+            var actual, base;
+            base = new punchcards.Base(cf, 'punchcard-base');
+            base.drawSvg();
+            base.drawBox();
+            actual = base.svg.select('g.chartbody-box').select('rect.chartbody-box')[0][0];
+            expect(actual).not.toBe(null);
+        });
 
 
-})
+});
 
 
